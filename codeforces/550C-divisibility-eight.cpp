6788045404f06d63d3a2c73062e66b3fdba9e292
Diego Cánez <canezdiego@gmail.com>
2020-02-26 20:22:34 -0300

    dp

---
+++ codeforces/550C-divisibility-eight.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+map<string, string> mem;
+
+bool is_divisible_eight(string n)
+{
+    int len = n.size();
+
+    if ((n[len - 1] - '0') % 2 == 1)
+        return false;
+    else if (len <= 3)
+        return (stoi(n) % 8) == 0;
+    else
+        return is_divisible_eight(n.substr(len - 3, 3));
+}
+
+string dp(string n)
+{
+    int    len = n.size();
+    string copy;
+
+    if (len == 0)
+        return "x";
+    if (len == 1 and (n[0] != '8' and n[0] != '0'))
+        return "x";
+    if (len > 1 and n[0] == '0')
+        return "x";
+    if (is_divisible_eight(n))
+        return n;
+
+    string &ans = mem[n];
+
+    if (ans != "")
+        return ans;
+
+    for (int i = max(0, len - 3); i < len; ++i)
+    {
+        copy = n;
+        ans  = dp(copy.erase(i, 1));
+        if (ans != "x")
+            return ans;
+    }
+    return ans;
+}
+
+int main(void)
+{
+
+    string n;
+
+    cin >> n;
+
+    while ((n[n.size() - 1] - '0') % 2 == 1)
+        n.pop_back();
+
+    string ans = dp(n);
+
+    if (ans == "x")
+        cout << "NO";
+    else
+        cout << "YES" << endl << ans;
+    cout << endl;
+
+    return 0;
+}
