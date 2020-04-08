1bb8e22527aeb2be34f4191ef1edb0a52eb89225
Diego Cánez <canezdiego@gmail.com>
2020-04-07 21:01:37 -0300

    Problem solution for my little brother uwu

---
+++ codeforces/112A-petya-and-strings.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool is_upper(char c) { return 'A' <= c and c <= 'Z'; }
+
+string lower(string s)
+{
+    string ans = s;
+
+    for (int i = 0, len = s.size(); i < len; ++i)
+    {
+        if (is_upper(s[i]))
+            ans[i] += ('a' - 'A');
+    }
+
+    return ans;
+}
+
+int main(void)
+{
+    string s1, s2;
+
+    cin >> s1 >> s2;
+
+    s1 = lower(s1);
+    s2 = lower(s2);
+
+    cout << (s1 == s2 ? 0 : (s1 > s2 ? 1 : -1)) << endl;
+
+    return 0;
+}
