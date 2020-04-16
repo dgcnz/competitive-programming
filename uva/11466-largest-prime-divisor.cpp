2d8e6cb1022e3f20f8d47dbd598675351a3a6c99
Diego Cánez <canezdiego@gmail.com>
2020-04-16 12:32:44 -0300

    uva 11466

---
+++ uva/11466-largest-prime-divisor.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+ll qn;
+
+ll trial_division(ll n)
+{
+    ll count     = 0;
+    ll max_prime = -1;
+
+    for (ll d = 2; d * d <= n; d++)
+    {
+        if (n % d == 0)
+            ++count;
+        while (n % d == 0)
+        {
+            n /= d;
+            max_prime = max(max_prime, d);
+        }
+    }
+    if (n > 1 and n != qn)
+    {
+        max_prime = max(max_prime, n);
+        ++count;
+    }
+    if (count >= 2)
+        return max_prime;
+    return -1;
+}
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    while (cin >> qn and qn)
+    {
+        cout << trial_division(abs(qn)) << endl;
+    }
+    return 0;
+}
