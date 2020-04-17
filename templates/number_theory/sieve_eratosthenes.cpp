c04ed64e735706b74837a0022ac33d3e07f99ce7
Diego Cánez <canezdiego@gmail.com>
2020-04-17 15:01:07 -0300

    uva 10006 10140 number theory + sieve in templates

--- templates/number_theory/sieve_eratosthenes.cpp
+++ templates/number_theory/sieve_eratosthenes.cpp
@@ -6,16 +6,16 @@ using ll  = long long;
 using vi  = vector<int>;
 using mii = map<int, int>;
 
-const int NMAX = 1e7 + 11;
+const int NMAX = 1e7 + 11; // okay for range <= INT_MAX
 
 bitset<NMAX> is_prime; // is_prime[i], true if number i is prime, else false
-vector<int>  prime;    // prime[i], prime at pos i
+vector<int>  primes;   // primes[i], prime at pos i
 
 vi prime_factors(ll n)
 {
     // mii factors;
     vi factors;
-    ll i = 0, pf = prime[i];
+    ll i = 0, pf = primes[i];
 
     while (pf * pf <= n)
     {
@@ -25,7 +25,7 @@ vi prime_factors(ll n)
             // ++factors[pf];
             n = n / pf;
         }
-        pf = prime[++i];
+        pf = primes[++i];
     }
 
     if (n != 1) // If n is prime then it will stay the same, add n.
@@ -33,18 +33,23 @@ vi prime_factors(ll n)
     return factors;
 }
 
-void sieve(ll maxn) // fi primes until maxn inclusive
+// Note it's only necessary to sieve until sqrt(upperbound), why?
+//
+// (1) All smaller numbers that are multiples of i necessary also have a prime
+// factor which is less than i, so all of them were already sifted earlier.
+
+void sieve(ll upperbound) // fi primes until upperbound inclusive
 {
-    ll sieve_size = maxn + 1;
+    ll sieve_size = upperbound + 1;
 
     is_prime.set(); // set all bits to true
     is_prime[0] = is_prime[1] = 0;
 
-    for (ll i = 2; i <= sieve_size; i++)
+    for (ll i = 2; i * i <= sieve_size; i++)
         if (is_prime[i])
         {
             for (ll j = i * i; j <= sieve_size; j += i)
                 is_prime[j] = 0;
-            prime.push_back((int)i);
+            primes.push_back((int)i);
         }
 }
