9536aaa58b0cd685786edd71b1760cc463310775
Diego Cánez <canezdiego@gmail.com>
2020-05-21 13:43:28 -0300

    K atcoder educational dp contest

---
+++ atcoder/dpK-stones.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+const int NMAX = 1e2 + 11;
+const int KMAX = 1e5 + 11;
+
+int  n, k, a[NMAX];
+bool vis[KMAX], mem[KMAX];
+
+bool wins(int stones)
+{
+    bool &ans = mem[stones];
+
+    if (vis[stones])
+        return ans;
+
+    vis[stones] = true;
+    for (int i = 0; i < n; ++i)
+        if (stones - a[i] >= 0 and not wins(stones - a[i]))
+            return (ans = true);
+    return (ans = false);
+}
+
+int main(void)
+{
+    cin >> n >> k;
+    for (int i = 0; i < n; ++i)
+        cin >> a[i];
+
+    cout << (wins(k) ? "First" : "Second") << endl;
+
+    return 0;
+}
