fdebc2d8aef8fc32575243f0bea05a724d0d50c4
Diego Cánez <canezdiego@gmail.com>
2020-05-22 21:24:56 -0300

    dp uva

--- uva/357-let-me-count-ways.cpp
+++ uva/357-let-me-count-ways.cpp
@@ -6,39 +6,27 @@ using ll = long long;
 
 const int NMAX = 3e4 + 11;
 
-int  coins[5] = {1, 5, 10, 25, 50};
-ll   mem[NMAX][5];
-bool vis[NMAX][5];
+int n, coins[5] = {1, 5, 10, 25, 50};
+ll  dp[NMAX];
 
-// dp(n, i) = number of ways to get n starting from the ith coin
-
-ll dp(int n, int i)
+ll solve(void)
 {
-    if (i >= 5 or n < 0)
-        return 0;
+    dp[0] = 1;
+    for (int i = 0; i < 5; ++i)
+        for (int j = coins[i]; j <= n; ++j)
+            dp[j] += dp[j - coins[i]];
 
-    if (vis[n][i])
-        return mem[n][i];
-
-    vis[n][i] = true;
-    ll &ans   = mem[n][i];
-
-    if (n == 0)
-        return (ans = 1);
-
-    return (ans = dp(n - coins[i], i) + dp(n, i + 1));
+    return dp[n];
 }
 
 int main(void)
 {
-    int n;
-    ll  m;
+    ll m;
 
     while (cin >> n)
     {
-        memset(mem, 0, sizeof mem);
-        memset(vis, 0, sizeof vis);
-        m = dp(n, 0);
+        memset(dp, 0, sizeof(dp));
+        m = solve();
         if (m == 1)
             cout << "There is only 1 way to produce ";
         else
