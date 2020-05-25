302d2f0cd8f4488e352092bf22434b3fa35b063e
Diego Cánez <canezdiego@gmail.com>
2020-05-25 14:21:23 -0300

    upsolving cf 664 div 3

---
+++ codeforces/1360D-buying-shovels.cpp
@@ -0,0 +1,36 @@
+#include <climits>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int solve(int n, int k)
+{
+    int ans = INT_MAX, d1, d2;
+    for (int i = 1, len = (int)sqrt(n); i <= len; ++i)
+    {
+        if (n % i == 0)
+        {
+            d1 = i;
+            d2 = n / i;
+
+            if (d2 <= k)
+                ans = min(ans, d1);
+            if (d1 <= k)
+                ans = min(ans, d2);
+        }
+    }
+    return ans;
+}
+
+int main(void)
+{
+    int t, n, k;
+    cin >> t;
+    while (t--)
+    {
+        cin >> n >> k;
+        cout << solve(n, k) << endl;
+    }
+    return 0;
+}
