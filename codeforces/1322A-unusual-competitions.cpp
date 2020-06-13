db1a5c555e4654700f48ce9c504ab418169d76ed
Diego Cánez <canezdiego@gmail.com>
2020-06-13 15:02:09 -0300

    gitgud and fenwick

---
+++ codeforces/1322A-unusual-competitions.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int    n;
+string s;
+
+int solve(void)
+{
+    int ans, open_br, l;
+    ans = open_br = 0;
+
+    for (int i = 0; i < n; ++i)
+    {
+        if (s[i] == '(')
+        {
+            open_br += 1;
+            if (open_br == 0)
+                ans += i - l + 1;
+        }
+        else
+        {
+            open_br -= 1;
+            if (open_br == -1)
+                l = i;
+        }
+    }
+    if (open_br != 0)
+        return -1;
+    return ans;
+}
+
+int main(void)
+{
+    cin >> n >> s;
+    cout << solve() << endl;
+
+    return 0;
+}
