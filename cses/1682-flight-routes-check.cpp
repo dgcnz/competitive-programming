bba619e280fb6f35a6cb00f1e772b5f71c68813e
Diego Cánez <canezdiego@gmail.com>
2020-07-30 14:16:11 -0300

    cses graphs

---
+++ cses/1682-flight-routes-check.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using vi = vector<int>;
+
+const int NMAX = 1e5 + 11;
+
+int n, m, x, y, visited[NMAX];
+vi  g[NMAX], gi[NMAX];
+
+void dfs(int u, const vi gr[], int vval)
+{
+    visited[u] = vval;
+
+    for (auto v : gr[u])
+        if (visited[v] != vval)
+            dfs(v, gr, vval);
+}
+
+bool vis_check(int vval, int &ans)
+{
+    for (int u = 1; u <= n; ++u)
+    {
+        if (visited[u] != vval)
+        {
+            ans = u;
+            return false;
+        }
+    }
+    return true;
+}
+
+bool solve(void)
+{
+    int vval;
+    dfs(1, g, vval = 1), x = 1;
+    if (not vis_check(vval, y))
+        return false;
+    dfs(1, gi, vval = 2), y = 1;
+    if (not vis_check(vval, x))
+        return false;
+    return true;
+}
+
+int main(void)
+{
+    int u, v;
+    cin >> n >> m;
+    for (int i = 0; i < m; ++i)
+    {
+        cin >> u >> v;
+        g[u].push_back(v);
+        gi[v].push_back(u);
+    }
+    if (solve())
+        cout << "YES" << endl;
+    else
+    {
+        cout << "NO" << endl;
+        cout << x << " " << y;
+    }
+
+    return 0;
+}
---
+++ cses/1750-planets-queries-I.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using vi = vector<int>;
+
+const int NMAX = 2e5 + 11;
+const int PTWO = 32;
+
+int _succ[NMAX][PTWO];
+
+int succ(int x, int k, int i = 0)
+{
+    if (k == 0)
+        return x;
+    if (k == 1)
+        return _succ[x][i];
+
+    int rem = k % 2;
+    if (rem == 1)
+        return _succ[succ(x, k / 2, i + 1)][i];
+    else
+        return succ(x, k / 2, i + 1);
+}
+
+int main(void)
+{
+    int n, q, x, k;
+    cin >> n >> q;
+
+    for (int u = 1; u <= n; ++u)
+        cin >> _succ[u][0];
+
+    for (int p = 1; p < PTWO; ++p)
+    {
+        for (int u = 1; u <= n; ++u)
+        {
+            _succ[u][p] = _succ[_succ[u][p - 1]][p - 1];
+        }
+    }
+
+    while (q--)
+    {
+        cin >> x >> k;
+        cout << succ(x, k) << endl;
+    }
+    return 0;
+}
