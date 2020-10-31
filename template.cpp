64dc4f785cb445ca97dc4b16b564ec360de7ad0f
Diego Cánez <canezdiego@gmail.com>
2020-10-31 17:12:30 -0500

    template

---
+++ template.cpp
@@ -0,0 +1,33 @@
+#ifdef DBG_MACRO_NO_WARNING
+#include <dbg.h>
+#endif
+#include <bits/stdc++.h>
+#define all(c) c.begin(), c.end()
+#define isz(c) (int)c.size()
+
+using namespace std;
+using ll = long long;
+using ii = pair<int, int>;
+using vi = vector<int>;
+
+template <typename InputIterator,
+          typename T = typename iterator_traits<InputIterator>::value_type>
+void read_n(InputIterator it, int n)
+{
+    copy_n(istream_iterator<T>(cin), n, it);
+}
+
+template <typename InputIterator,
+          typename T = typename iterator_traits<InputIterator>::value_type>
+void write(InputIterator first, InputIterator last, const char *delim = "\n")
+{
+    copy(first, last, ostream_iterator<T>(cout, delim));
+}
+
+int main(void)
+{
+    ios::sync_with_stdio(false), cin.tie(NULL);
+    int t;
+    cin >> t;
+    return 0;
+}
