9019bd18f826a9ee978bac275a7df0efa30883e3
Diego Canez <canezdiego@gmail.com>
2020-12-17 13:59:33 -0500

    updated template

--- template.cpp
+++ template.cpp
@@ -19,6 +19,8 @@
 
 #ifdef DBG_MACRO_NO_WARNING
 #include <dbg.h>
+#else
+#define dbg(e) (e)
 #endif
 #include <bits/stdc++.h>
 #define all(c) begin(c), end(c)
@@ -28,20 +30,6 @@ using namespace std;
 using ll = long long;
 using ii = pair<int, int>;
 using vi = vector<int>;
-
-template <typename InputIterator,
-          typename T = typename iterator_traits<InputIterator>::value_type>
-void read_n(InputIterator it, int n)
-{
-    copy_n(istream_iterator<T>(cin), n, it);
-}
-
-template <typename InputIterator,
-          typename T = typename iterator_traits<InputIterator>::value_type>
-void write(InputIterator first, InputIterator last, const char *delim = "\n")
-{
-    copy(first, last, ostream_iterator<T>(cout, delim));
-}
 
 int main(void)
 {
