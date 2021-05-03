accd94c8dd2d667e8f03102f9422192c545f1701
Diego Canez <canezdiego@gmail.com>
2021-05-03 13:02:53 -0500

    cf

---
+++ codeforces/938B-run-for-your-prize.cpp
@@ -0,0 +1,33 @@
+#ifdef DBG_MACRO_NO_WARNING
+#include <dbg.h>
+#else
+#define dbg(...) ((void)0)
+#endif
+#include <bits/stdc++.h>
+#include <cplib/utils/io>
+#define all(c) begin(c), end(c)
+#define isz(c) (int)(c).size()
+
+using namespace std;
+using namespace cplib;
+using ll = long long;
+using ii = pair<int, int>;
+using vi = vector<int>;
+
+int main(void)
+{
+    ios::sync_with_stdio(false), cin.tie(NULL);
+    int n;
+    cin >> n;
+    vi a(n);
+    read(all(a));
+
+    sort(all(a));
+
+    int ans = min(a[n - 1] - 1, ((int)1e6 - a[0]));
+    for (int i = 0; i < n - 1; ++i)
+        ans = min(ans, max(a[i] - 1, (int)1e6 - a[i + 1]));
+
+    cout << ans << endl;
+    return 0;
+}
