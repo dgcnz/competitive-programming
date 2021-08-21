f536d5c039d4ac1a724e6bb42251e1481a23fd4a
Diego Canez <canezdiego@gmail.com>
2021-08-21 11:36:07 -0500

    updated template

--- template.cpp
+++ template.cpp
@@ -1,32 +1,15 @@
-/** @file
- * @date                YY-MM-DD
- * @url                 https://url.com
- * @tags                math, sorting
- * @status              AC
- * @score               0
- * @difficulty          0
- * @editorial           no
- * @reading_time        000
- * @thinking_time       000
- * @coding_time         000
- * @time_complexity     O(n)
- * @memory_complexity   O(n)
- * @idea
- *
- *
- * @endidea
- */
-
 #ifdef DBG_MACRO_NO_WARNING
 #include <dbg.h>
 #else
 #define dbg(...) ((void)0)
 #endif
 #include <bits/stdc++.h>
+#include <cplib/utils/io>
 #define all(c) begin(c), end(c)
 #define isz(c) (int)(c).size()
 
 using namespace std;
+using namespace cplib;
 using ll = long long;
 using ii = pair<int, int>;
 using vi = vector<int>;
@@ -36,5 +19,8 @@ int main(void)
     ios::sync_with_stdio(false), cin.tie(NULL);
     int t;
     cin >> t;
+    while (t--)
+    {
+    }
     return 0;
 }
